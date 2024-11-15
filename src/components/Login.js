5c05cb49745514a1cf20a5c2b26acde2a40b5c4e
kadam71 <kadam71@avaya.com>
2024-11-15 14:09:05 +0530

    Updates

--- src/components/Login.js
+++ src/components/Login.js
@@ -9,7 +9,7 @@ function Login() {
         onSuccess={credentialResponse => {
           console.log("🚀 ~ Login ~ credentialResponse 1:", credentialResponse)
           document.cookie = "sharedData=" + encodeURIComponent(JSON.stringify(credentialResponse));
-          window.location.replace(`http://172.25.12.211:8080/ruleeditor-0.0.1-SNAPSHOT?credential=${JSON.stringify(credentialResponse)}`);
+          window.location.replace(`http://172.25.12.211:8080/ruleeditor-0.0.1-SNAPSHOT?credential="${JSON.stringify(credentialResponse)}"`);
         }}
         onError={err => {
           console.log("🚀 ~ Login ~ err:", err)
