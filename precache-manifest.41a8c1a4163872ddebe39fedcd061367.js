7d0a9b4c08167c709cbb0cb729d2f1b3e01174fe
kadam71 <kadam71@avaya.com>
2024-11-15 11:19:33 +0530

    Updates

---
+++ precache-manifest.41a8c1a4163872ddebe39fedcd061367.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef4520e5717ee962587c74b7f3630ac7",
+    "url": "/react-google-authentication/index.html"
+  },
+  {
+    "revision": "df44e946c2e17527264d",
+    "url": "/react-google-authentication/static/css/main.f072c70a.chunk.css"
+  },
+  {
+    "revision": "2e4166f03d98686049f8",
+    "url": "/react-google-authentication/static/js/2.5b02833e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-google-authentication/static/js/2.5b02833e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df44e946c2e17527264d",
+    "url": "/react-google-authentication/static/js/main.8070cfe8.chunk.js"
+  },
+  {
+    "revision": "ac12cda5d4a95756c952",
+    "url": "/react-google-authentication/static/js/runtime-main.000499cd.js"
+  }
+]);
